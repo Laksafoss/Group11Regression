f2f0515821df0238057ca86cfdc6db9e1916d0b4
Laksafoss <anna@kerubia.dk>
2019-02-28 17:21:22 +0100

    Update 03_modeling.R

--- 03_modeling.R
+++ 03_modeling.R
@@ -1,5 +1,7 @@
 
 library(glmnet)
+library(ggplot2)
+library(gridExtra)
 
 #DATA <- readRDS("outputs/narwhal.RDS")
 #DATA <- readRDS("outputs/narwhal_Minut.RDS")
@@ -21,14 +23,13 @@ xsub <- model.matrix(~ Phasesub + Area + Ind + Los + Sun + ODBA + Dive + Acou.qu
 
 
 
-# setup
+# Setup for Ridge regression ===================================================
 n <- dim(x)[1]
 p <- dim(x)[2] - 1
 train.size <- round(n/10)
 train <- sample(1:n,train.size,replace=FALSE)
 
-
-fitter <- function(x, y, train) {
+netfitter <- function(x, y, data, train) {
   x.train <- x[train, ]
   y.train <- y[train]
   
@@ -38,10 +39,10 @@ fitter <- function(x, y, train) {
   fit <- glmnet(x.train, y.train, family = "poisson", alpha = 0)
   cv <- cv.glmnet(x.train, y.train, family = "poisson", alpha = 0)
   rmse <- sqrt(mean((predict(fit, newx = x.test, s = cv$lambda.min) - y.test)^2))
-  return(structure(list(fit = fit, cv = cv, rmse = rmse), class = "fitterglm"))
+  return(structure(list(fit = fit, cv = cv, rmse = rmse), class = "netfitter"))
 }
 
-plot.fitterglm <- function(x) {
+plot.netfitter <- function(x) {
   par(mfrow=c(2,1))
   plot(x$fit, xvar = "lambda"); abline(v = log(x$cv$lambda.min))
   plot(x$cv); abline(v = log(x$cv$lambda.min))
@@ -49,7 +50,33 @@ plot.fitterglm <- function(x) {
 }
 
 
-# Depth model ------------------------------------------------------------------
+
+# Setup for normal regrssion ===================================================
+residualplotter <- function(fit, n, m) {
+  dat <- fit$data
+  index <- sample(n*m, 1)
+  plots <- lapply(seq_len(n * m), function(i) {
+    if (i == index) {
+      true <- ggplot2::fortify(fit)
+      ggplot2::qplot(.fitted, .resid, data = true) + ggplot2::geom_smooth()
+    } else {
+      y <- simulate(fit)[,1]
+      form <- as.formula(
+        paste0("y ~ ", grep("~", fit$formula, value = T, invert = T)[2]))
+      glm <- glm(form, data = cbind(y = y, dat), family = "poisson")
+      Diag <- ggplot2::fortify(glm)
+      ggplot2::qplot(.fitted, .resid, data = Diag) + ggplot2::geom_smooth()
+    }
+  })
+  print(index)
+  marrangeGrob(plots, ncol = n, nrow = m)
+}
+
+
+
+# Depth model ==================================================================
+
+# Ridge regression
 no <- which(colnames(x) %in% c("DiveTRUE","Acou.qua"))
 fit.depth <- fitter(x[,-no], DATA$Depth, train)
 plot(fit.depth)
@@ -57,29 +84,79 @@ plot(fit.depth)
 fit.depth.sub <- fitter(xsub[,-no], DATA$Depth, train)
 plot(fit.depth.sub)
 
+# normal regression
+fit.depth <- glm(Depth ~ Phase + Area + Ind + Los + Sun + ODBA, data = DATA, family = "poisson")
+fit.depth.sub <- glm(Depth ~ Phasesub + Area + Ind + Los + Sun + ODBA, data = DATA, family = "poisson")
 
-# Click model ------------------------------------------------------------------
-fit.click <- fitter(x, DATA$Click, train)
+anova(fit.depth, fit.depth.sub, test = "LRT") # Likelihood ratio test
+anova(fit.depth, fit.depth.sub, test = "Cp")  # like AIC
+
+residualplotter(fit.depth, 3,3)
+residualplotter(fit.depth.sub, 3,3)
+
+
+
+# Click model ==================================================================
+
+# Ridge regression
+fit.click <- netfitter(x, DATA$Click, train)
 plot(fit.click)
 
-fit.click.sub <- fitter(xsub, DATA$Click, train)
+fit.click.sub <- netfitter(xsub, DATA$Click, train)
 plot(fit.click.sub)
 
 
-# Call model -------------------------------------------------------------------
-fit.call <- fitter(x, DATA$Call, train)
+# normal regression
+fit.click <- glm(Click ~ Phase + Area + Ind + Los + Sun + ODBA, data = DATA, family = "poisson")
+fit.click.sub <- glm(Click ~ Phasesub + Area + Ind + Los + Sun + ODBA, data = DATA, family = "poisson")
+
+anova(fit.click, fit.click.sub, test = "LRT") # Likelihood ratio test
+anova(fit.click, fit.click.sub, test = "Cp")  # like AIC
+
+residualplotter(fit.click, 3,3)
+residualplotter(fit.click.sub, 3,3)
+
+
+
+# Call model ===================================================================
+
+# Ridge regression
+fit.call <- netfitter(x, DATA$Call, train)
 plot(fit.call)
 
-fit.call.sub <- fitter(xsub, DATA$Call, train)
+fit.call.sub <- netfitter(xsub, DATA$Call, train)
 plot(fit.call.sub)
 
 
-# Strokerate model -------------------------------------------------------------
+# normal regression
+fit.call <- glm(Call ~ Phase + Area + Ind + Los + Sun + ODBA, data = DATA, family = "poisson")
+fit.call.sub <- glm(Call ~ Phasesub + Area + Ind + Los + Sun + ODBA, data = DATA, family = "poisson")
+
+anova(fit.call, fit.call.sub, test = "LRT") # Likelihood ratio test
+anova(fit.call, fit.call.sub, test = "Cp")  # like AIC
+
+residualplotter(fit.call, 3,3)
+residualplotter(fit.call.sub, 3,3)
+
+
+
+# Strokerate model =============================================================
+
+# Ridge regression
 no <- which(colnames(x) %in% c("DiveTRUE","Acou.qua"))
-fit.strokerate <- fitter(x[,-no], DATA$StrokeRate, train)
+fit.strokerate <- netfitter(x[,-no], DATA$StrokeRate, train)
 plot(fit.strokerate)
 
-fit.strokerate.sub <- fitter(xsub[,-no], DATA$StrokeRate, train)
+fit.strokerate.sub <- netfitter(xsub[,-no], DATA$StrokeRate, train)
 plot(fit.strokerate.sub)
 
 
+# normal regression
+fit.strokerate <- glm(StrokeRate ~ Phase + Area + Ind + Los + Sun + ODBA, data = DATA, family = "poisson")
+fit.strokerate.sub <- glm(StrokeRate ~ Phasesub + Area + Ind + Los + Sun + ODBA, data = DATA, family = "poisson")
+
+anova(fit.strokerate, fit.strokerate.sub, test = "LRT") # Likelihood ratio test
+anova(fit.strokerate, fit.strokerate.sub, test = "Cp")  # like AIC
+
+residualplotter(fit.strakerate, 3,3)
+residualplotter(fit.strakerate.sub, 3,3)
